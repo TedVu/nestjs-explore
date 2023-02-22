669557d006297288cbe4f7e811d0b92d85159d4f
TedEngineer <tedvu184@gmail.com>
2023-02-22 16:55:30 +1100

    Log json message to the endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  test() {
+    return 'Hello World!';
+  }
+
+  signin() {
+    return { msg: 'I have signed in' };
+  }
+
+  signup() {
+    return { msg: 'I have signed up' };
+  }
+}
