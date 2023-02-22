fe3aefe56774b54e2a046c2bb33e5ca29635a102
TedEngineer <tedvu184@gmail.com>
2023-02-22 16:39:47 +1100

    Fix prettier config and add file structure of a complete service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-@Module({})
-class AuthModule {}
+@Module({ controllers: [AuthController], providers: [AuthService] })
 export class AuthModule {}
